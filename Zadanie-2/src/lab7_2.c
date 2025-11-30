07158f6bb663ec4e36838f0c74cbd35b2de38b9e
Piotr <79946580+motocyklista@users.noreply.github.com>
2025-11-30 15:11:38 +0100

    Implement matrix input and transpose output
    Added functionality to read a variable-sized matrix and print its transpose.

--- Zadanie-2/src/lab7_2.c
+++ Zadanie-2/src/lab7_2.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
 
 int main(void) {
-   
+    int R, C;
+
+    // Wczytanie liczby wierszy i kolumn
+    if (scanf("%d %d", &R, &C) != 2 || R < 1 || C < 1) {
+        printf("BLAD DANYCH WEJSCIOWYCH\n");
+        return 1;
+    }
+
+    // Tworzenie macierzy o zmiennym rozmiarze (VLA)
+    int matrix[R][C];
+
+    // Wczytanie elementów macierzy
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            if (scanf("%d", &matrix[i][j]) != 1) {
+                printf("BLAD DANYCH WEJSCIOWYCH\n");
+                return 1;
+            }
+        }
+    }
+
+    // Wypisanie macierzy transponowanej
+    for (int j = 0; j < C; j++) {
+        for (int i = 0; i < R; i++) {
+            printf("%d", matrix[i][j]);
+            if (i != R - 1) printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
